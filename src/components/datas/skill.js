40d0f3b48d78134f11ec987fa2f900e7544cb10e
Jerome Franklin X <118640107+JEROME-1122@users.noreply.github.com>
2025-09-13 18:09:37 +0530

    Update skill.js

--- src/components/datas/skill.js
+++ src/components/datas/skill.js
@@ -1,64 +1,87 @@
+import html from "../../assets/skills/html_5.png"
+import css from "../../assets/skills/css_3.png"
+import bootstrap from "../../assets/skills/bootstrap.png"
+import Tailwind_CSS from "../../assets/skills/Tailwind_CSS.png"
+import js from "../../assets/skills/js.png"
+import react from "../../assets/skills/react.png"
+import angular from "../../assets/skills/angular.png"
+import nodejs from "../../assets/skills/nodejs.png"
+import express_js from "../../assets/skills/express_js.png"
+import mongodb from "../../assets/skills/mongodb.png"
+import php from "../../assets/skills/php.png"
+import mysql from "../../assets/skills/mysql.png"
+import github from "../../assets/skills/github.png"
+import postman from "../../assets/skills/postman.png"
+import vs_code from "../../assets/skills/vs_code.png"
+import photoshop from "../../assets/skills/photoshop.png"
+import illustrator from "../../assets/skills/illustrator.png"
+import indesign from "../../assets/skills/indesign.png"
+import figma from "../../assets/skills/figma.png"
+import xd from "../../assets/skills/xd.png"
+import dreamweaver from "../../assets/skills/dreamweaver.png"
+
+
+
 export const skills = [
   {
     id: 1,
     names: "HTML",
-    img:"../../../assets/skills/html_5.png"
+    img:`${html}`
   },
   {
     id: 2,
     names: "CSS",
-    img:"src/assets/skills/css_3.png"
+    img:`${css}`
   },
   {
     id: 3,
     names: "Bootstrap",
-    img:"src/assets/skills/bootstrap.png"
+    img:`${bootstrap}`
   },
   {
     id: 4,
     names: "Tailwind CSS",
-     img:"src/assets/skills/Tailwind_CSS.png"
+     img:`${Tailwind_CSS}`
   },
   {
     id: 5,
     names: "JavaScript",
-    img:"src/assets/skills/js.png"
+    img:`${js}`
   },
   {
     id: 6,
     names: "React",
-    img:"src/assets/skills/react.png"
+    img:`${react}`
   },
   {
     id: 7,
     names: "Angular",
-    img:"src/assets/skills/angular.png"
+    img:`${angular}`
   },
   {
     id: 8,
     names: "Node.js",
-    img:"src/assets/skills/nodejs.png"
+    img:`${nodejs}`
   },
-
   {
     id: 9,
     names: "Express",
-    img:"src/assets/skills/express_js.png"
+    img:`${express_js}`
   },
   {
     id: 10,
     names: "MongoDB",
-        img:"src/assets/skills/mongodb.png"
+        img:`${mongodb}`
   },
     {
     id: 11,
     names: "Php",
-    img:"src/assets/skills/php.png"
+    img:`${php}`
   },
   {
     id: 12,
     names: "MySQL",
-    img:"src/assets/skills/mysql.png"
+    img:`${mysql}`
   },
   // {
   //   id: 12,
@@ -69,43 +92,47 @@ export const skills = [
   {
     id: 13,
     names: "GitHub",
-    img:"src/assets/skills/github.png"
+    img:`${github}`
+  },
+   {
+    id: 14,
+    names: "Postman",
+    img:`${postman}`
   },
   {
-    id: 14,
+    id: 15,
     names: "VS Code",
-    img:"src/assets/skills/vs_code.png"
+    img:`${vs_code}`
   },
   
   {
-    id: 15,
-    names: "Photoshop",
-    img:"src/assets/skills/photoshop.png"
-  },
-  {
     id: 16,
-    names: "Illustrator",
-    img:"src/assets/skills/illustrator.png"
+    names: "Photoshop",
+    img:`${photoshop}`
   },
   {
     id: 17,
-    names: "Indesign",
-    img:"src/assets/skills/indesign.png"
+    names: "Illustrator",
+    img:`${illustrator}`
   },
   {
     id: 18,
-    names: "Figma",
-    img:"src/assets/skills/figma.png"
-  },
-   {
-    id: 19,
-    names: "XD",
-    img:"src/assets/skills/xd.png"
+    names: "Indesign",
+    img:`${indesign}`
   },
   {
+    id: 19,
+    names: "Figma",
+    img:`${figma}`
+  },
+   {
     id: 20,
+    names: "XD",
+    img:`${xd}`
+  },
+  {
+    id: 21,
     names: "Dreamweaver",
-    img:"src/assets/skills/dreamweaver.png"
+    img:`${dreamweaver}`
   },
 ];
-
