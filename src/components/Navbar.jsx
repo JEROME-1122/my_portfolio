89c22a279d311ce0311a0790cf6399a835716e67
Jerome Franklin X <118640107+JEROME-1122@users.noreply.github.com>
2025-12-05 18:35:33 +0530

    Refactor Navbar component for improved structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ export default function Navbar() {
   return (
     <>
       <nav className="fixed top-0 w-full  text-white shadow-lg z-50 lg:px-0 px-[0%] bg-gradient-to-r from-[#1F1E38] to-[#281C37]">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="container mx-auto ">
           <div className="flex justify-between items-center h-16">
             {/* Logo */}
             <div className="text-2xl font-bold">MyPortfolio</div>
